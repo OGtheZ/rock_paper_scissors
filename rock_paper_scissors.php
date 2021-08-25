f232b6b12ec14783d4a5835c63e546d40ffc75db
OGtheZ <zzaicevsedg@gmail.com>
2021-08-25 10:47:00 +0300

    new changes

--- rock_paper_scissors.php
+++ rock_paper_scissors.php
@@ -6,7 +6,7 @@ $computersChoice = $choice[rand(0,2)];
 
 $input = (int) readline("Enter a choice - 0 : rock, 1 : Paper, 2 : Scissors: ");
 if ($input < 0 || $input > 2) {
-    echo "The choice must be between 0 and 2";
+    echo "The choice must be between 0 and 2" . PHP_EOL;
     exit;
 } else {
     $playersChoice = $choice[$input];
